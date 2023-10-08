26b0ec7d2b40b9eb9babb791442e8bb6dd3242be
Jani Nykänen <jnyknn@gmail.com>
2023-10-08 13:55:04 +0300

    Shop tiles

---
+++ tiled/tileset_caves.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_caves" tilewidth="16" tileheight="16" tilecount="32" columns="16">
+ <image source="../assets/bitmaps/tileset_caves.png" width="256" height="32"/>
+</tileset>
