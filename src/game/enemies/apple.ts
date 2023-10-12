ce3a9750a9bbaa176fb92e26c84a1a161b479b48
Jani Nykänen <jnyknn@gmail.com>
2023-10-12 22:00:11 +0300

    Apple enemy

---
+++ src/game/enemies/apple.ts
@@ -0,0 +1,54 @@
+import { ProgramEvent } from "../../core/event.js";
+import { Flip } from "../../gfx/interface.js";
+import { Rectangle } from "../../math/rectangle.js";
+import { Player } from "../player.js";
+import { TILE_WIDTH } from "../tilesize.js";
+import { Enemy } from "./enemy.js";
+
+
+
+export class Apple extends Enemy {
+
+
+    
+    protected init() : void {
+        
+        this.damage = 2;
+
+        this.maxHealth = 5;
+
+        this.spr.setFrame(0, 5);
+
+        this.dir = (Math.floor(this.pos.x/TILE_WIDTH)) % 2 == 0 ? 1 : -1;
+
+        this.collisionBox = new Rectangle(0, 2, 12, 12);
+        this.hitbox = this.collisionBox.clone();
+
+        this.target.y = 0;
+
+        this.getGravity = false;
+    }
+
+
+    protected updateAI(event : ProgramEvent) : void {
+        
+        const MOVE_SPEED : number = 0.25;
+        const WAVE_SPEED : number = Math.PI*2/120.0;
+
+        this.spr.animate(5, 0, 3, 6, event.tick);
+
+        this.flip = this.dir > 0 ? Flip.Horizontal : Flip.None;
+
+        this.specialTimer = (this.specialTimer + WAVE_SPEED*event.tick) % (Math.PI*2);
+
+        this.target.x = this.dir*MOVE_SPEED;
+
+        this.target.y = Math.sin(this.specialTimer)*0.25;
+    }
+
+
+    protected horizontalCollisionEvent(dir: 1 | -1, event : ProgramEvent): void {
+        
+        this.dir *= -1;
+    }
+}
--- src/game/enemies/enemytype.ts
+++ src/game/enemies/enemytype.ts
@@ -1,11 +1,12 @@
 import { clamp } from "../../math/utility.js";
-import { Slime } from "./slime.js"
-import { Rabbit } from "./rabbit.js"
-import { Turtle } from "./turtle.js"
-import { Caterpillar } from "./catepillar.js"
+import { Slime } from "./slime.js";
+import { Rabbit } from "./rabbit.js";
+import { Turtle } from "./turtle.js";
+import { Caterpillar } from "./catepillar.js";
+import { Apple } from "./apple.js";
 
 
-const ENEMY_TYPES : Function[] = [Slime, Rabbit, Caterpillar, Turtle];
+const ENEMY_TYPES : Function[] = [Slime, Rabbit, Caterpillar, Turtle, Apple];
 
 
 export const getEnemyType = (id : number) => ENEMY_TYPES[clamp(id, 0, ENEMY_TYPES.length-1)];
